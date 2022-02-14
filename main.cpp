78026af4ec6f014f1a8f1c20b398fe96183f4390
Gabriel <gabrieltsf10@gmail.com>
2022-02-14 12:23:42 -0300

    Adicionando um executável com uma imagem importada

--- main.cpp
+++ main.cpp
@@ -8,16 +8,16 @@ FLAGS NECESSÁRIAS PARA O COMPILADOR/LINKADOR -> -lallegro -lallegro_image -lall
 
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
-#include <allegro5/allegro_font.h>   //Importação das funções de FONTE do allegro -> usar junto com 'allegro_ttf.h' para carregar fontes ttf(TrueType Font) 
-#include <allegro5/allegro_ttf.h>    //Ao importar esses dois arquivos, adicionar as flags -lallegro_font -lallegro_ttf ao linkador
+#include <allegro5/allegro_font.h>   
+#include <allegro5/allegro_ttf.h>    
 #include <iostream>
 #include <stdlib.h>
 
 using namespace std;
 
 const float FPS = 10;
-const int SCREEN_W = 500;
-const int SCREEN_H = 550;
+const int SCREEN_W = 950;
+const int SCREEN_H = 950;
 const int QUAD_SIZE = 20;
 
 
@@ -26,6 +26,7 @@ ALLEGRO_FONT *font = NULL;        // Ponteiro de FONTE para inicializar o objeto
 ALLEGRO_DISPLAY *display = NULL;
 ALLEGRO_EVENT_QUEUE *event_queue = NULL;
 ALLEGRO_TIMER *timer = NULL;
+ALLEGRO_BITMAP *menu = NULL;
 
 
 
@@ -75,6 +76,7 @@ int inicializa() {
 
         return 0;
     }
+    menu = al_load_bitmap("CandyCrushResources/menu.bmp");
     al_init_font_addon();    // INICIALIZAR AS FUNÇÕES DE FONTE 
     al_init_ttf_addon();     // INICIALIZAR ESTE ADDON -APÓS- INICIALIZAR O font_addon
     al_register_event_source(event_queue, al_get_display_event_source(display));
@@ -98,7 +100,7 @@ int main(int argc, char **argv)
        		
 			redraw = false;
             al_clear_to_color(al_map_rgb(0,255,0));
-
+            al_draw_bitmap(menu, 0,0,0);
 	
 			al_flip_display();
         
