9151db76ad2ee1d25482a74685bf891f9bf553d4
Libiane Gomes <84982225+Libianegomes@users.noreply.github.com>
2022-03-01 13:00:11 -0300

    Correcao de alguns bugs

--- src/main.cpp
+++ src/main.cpp
@@ -5,23 +5,22 @@
 */
 
 
+
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #include <allegro5/allegro_font.h>   
-#include <allegro5/allegro_ttf.h> 
+#include <allegro5/allegro_ttf.h>    
 #include <iostream>
 #include <stdlib.h>
 
-#include "main.h"   
-
-using namespace std;
+#include "main.h"
 
 const float FPS = 10;
-const int SCREEN_W = 950;
-const int SCREEN_H = 950;
+const int SCREEN_W = 1000;
+const int SCREEN_H = 561;
 const int QUAD_SIZE = 20;
-const int OFFSETX = 240;
-const int OFFSETY = 210;
+const int OFFSETX = 500;
+const int OFFSETY = 100;
 
 const char *nome = NULL;
 
@@ -36,8 +35,14 @@ ALLEGRO_BITMAP *dlaranja = NULL;
 ALLEGRO_BITMAP *droxo = NULL;
 ALLEGRO_BITMAP *dverde = NULL;
 ALLEGRO_MOUSE_CURSOR *cursor = NULL;
-
-
+ALLEGRO_BITMAP *Pirulito = NULL;
+ALLEGRO_BITMAP *fundosugar = NULL;
+ALLEGRO_BITMAP *Festa = NULL;
+ALLEGRO_BITMAP *fundorosa = NULL;
+ALLEGRO_BITMAP *Maozinha = NULL;
+ALLEGRO_BITMAP *Muda1 = NULL;
+ALLEGRO_BITMAP *Nave = NULL;
+ALLEGRO_BITMAP *fundo = NULL;
 bool redraw = true;   
 bool sair = false;
 bool pressao=false;
@@ -54,40 +59,40 @@ int inicializa() {
     
     if(!al_init())
     {
-        cout << "Falha ao carregar Allegro" << endl;
+        std::cout << "Falha ao carregar Allegro" << std::endl;
         return 0;
     }
 
     if(!al_install_keyboard())
     {
-        cout << "Falha ao inicializar o teclado" << endl;
+        std::cout << "Falha ao inicializar o teclado" << std::endl;
         return 0;
     }
 
     timer = al_create_timer(1.0 / FPS);
     if(!timer)
     {
-        cout << "Falha ao inicializar o temporizador" << endl;
+        std::cout << "Falha ao inicializar o temporizador" << std::endl;
         return 0;
     }
 
     if(!al_init_image_addon())
     {
-        cout <<"Falha ao iniciar al_init_image_addon!" << endl;
+        std::cout <<"Falha ao iniciar al_init_image_addon!" << std::endl;
         return 0;
     }
     display = al_create_display(SCREEN_W, SCREEN_H);
     if(!display)
     {
-        cout << "Falha ao inicializar a tela" << endl;
+        std::cout << "Falha ao inicializar a tela" << std::endl;
         al_destroy_timer(timer);
         return 0;
     }
 
-    mapa = al_load_bitmap("assets/tab.bmp");
+    mapa = al_load_bitmap("assets/tabuleiro.bmp");
     if(!mapa)
     {
-        cout << "Falha ao carregar o mapa!" << endl;
+        std::cout << "Falha ao carregar o mapa!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
@@ -95,23 +100,23 @@ int inicializa() {
     mouse=al_load_bitmap("assets/personagem.png"); 
     if(!al_install_mouse())
     {
-        cout << "Falha ao inicializar o mouse" << endl;
+        std::cout << "Falha ao inicializar o mouse" << std::endl;
         return 0;
     }
 
-    
+
 
     event_queue = al_create_event_queue();
     if(!event_queue)
     {
-        cout << "Falha ao criar a fila de eventos" << endl;
+        std::cout << "Falha ao criar a fila de eventos" << std::endl;
         al_destroy_display(display);
 
         return 0;
     }
     menu = al_load_bitmap("assets/menu.bmp");
     if(!menu){
-        cout << "FALHA AO CARREGAR O MENU" << endl;
+        std::cout << "FALHA AO CARREGAR O MENU" << std::endl;
 
         al_destroy_display(display);
         
@@ -120,7 +125,7 @@ int inicializa() {
     dlaranja = al_load_bitmap("assets/dlaranja.tga"); //carrega a imagem da cabeca
 	if(!dlaranja)
     {
-        cout << "Falha ao carregar o doce laranja!" << endl;
+        std::cout << "Falha ao carregar o doce laranja!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
@@ -129,23 +134,71 @@ int inicializa() {
     droxo = al_load_bitmap("assets/droxo.tga"); //carrega a imagem da cabeca
 	if(!droxo)
     {
-        cout << "Falha ao carregar o doce roxo" << endl;
+        std::cout << "Falha ao carregar o doce verde" << std::endl;
         al_destroy_display(display);
         return 0;
     }
 
-    cursor = al_create_mouse_cursor(droxo, 0, 0);
-    if(!cursor)
-        {
-            cout << "Falha ao carregar o cursor" << endl;
-            al_destroy_mouse_cursor(cursor);
-            return 0;
-        }
-
-    dverde = al_load_bitmap("assets/dverde.tga"); //carrega a imagem da cabeca
-	if(!droxo)
+     Pirulito = al_load_bitmap("assets/Pirulito.bmp");
+    if(!Pirulito)
     {
-        cout << "Falha ao carregar o doce verde" << endl;
+        std::cout << "Falha ao carregar o pirulito!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    fundosugar = al_load_bitmap("assets/fundosugar.bmp");
+    if(!fundosugar)
+    {
+        std::cout << "Falha ao carregar o Fundo do doce!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    Festa = al_load_bitmap("assets/festa.bmp");
+    if(!Festa)
+    {
+        std::cout << "Falha ao carregar ajuda festa!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    fundorosa = al_load_bitmap("assets/fundorosa.bmp");
+    if(!fundorosa)
+    {
+        std::cout << "Falha ao carregar fundo das ajudas!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    Maozinha = al_load_bitmap("assets/Maozinha.bmp");
+    if(!Maozinha)
+    {
+        std::cout << "Falha ao carregar ajuda mao!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    Muda1 = al_load_bitmap("assets/Muda.bmp");
+    if(!Muda1)
+    {
+        std::cout << "Falha ao carregar ajuda que muda os doces!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    Nave = al_load_bitmap("assets/Nave.bmp");
+    if(!Nave)
+    {
+        std::cout << "Falha ao carregar ajuda nave!" << std::endl;
+        al_destroy_display(display);
+        return 0;
+    }
+
+    fundo = al_load_bitmap("assets/Fundo2.bmp");
+    if(!Nave)
+    {
+        std::cout << "Falha ao carregar fundo!" << std::endl;
         al_destroy_display(display);
         return 0;
     }
@@ -174,12 +227,12 @@ int jogo(ALLEGRO_EVENT &ev){
 	        //<------------------------------------------------------->
             else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE){
                 sair=true;
-                break;
-            }
+                        break;
+                    }
             else if(ev.type == ALLEGRO_EVENT_KEY_UP){
                 if(ev.keyboard.keycode==ALLEGRO_KEY_ESCAPE){
-                    break;
-                }
+                        break;
+                    }
             }
             else if(ev.type== ALLEGRO_EVENT_MOUSE_AXES){ //SE PASSAR O PONTEIRO EM CIMA DO DISPLAY
 
@@ -187,12 +240,12 @@ int jogo(ALLEGRO_EVENT &ev){
                     mouse_y=(ev.mouse.y);
 
 
-                    cout<<mouse_x<<" "<< mouse_y<<endl;
+                    std::cout<<mouse_x<<" "<< mouse_y<<std::endl;
             }
             else if (ev.type== ALLEGRO_EVENT_MOUSE_BUTTON_DOWN){ //se der o click no mouse
                 if(ev.mouse.button & 1){ // &1 = botão esquerdo
                     pressao=true;   //pressionado= TRUE
-                    cout<<"CLICK"<<endl;
+                    std::cout<<"CLICK"<<std::endl;
                 }
 
             }
@@ -206,15 +259,23 @@ int jogo(ALLEGRO_EVENT &ev){
                 redraw = false;
 
                 al_clear_to_color(al_map_rgb(0,0,0));
+                al_draw_bitmap(dlaranja,mouse_x,mouse_y,0);  //vai desenhar uma peça laranha no mouse, apenas para teste nao achei um ponteiro top ainda kkkkkk
 
+                al_draw_bitmap(fundo,0,0,0);
                 al_draw_bitmap(mapa,0+OFFSETX,0+OFFSETY,0);
+                al_draw_bitmap(fundorosa,380,450,100);
+                al_draw_bitmap(Maozinha,400,465,100);
+                al_draw_bitmap(Nave,440,465,100);
+                al_draw_bitmap(Pirulito,485,462,100);
+                al_draw_bitmap(Festa,530,469,100);
 
-                //al_draw_bitmap(dlaranja,mouse_x,mouse_y,0); //vai desenhar uma peça laranha no mouse, apenas para teste nao achei um ponteiro top ainda kkkkkk
                 al_set_mouse_cursor(display, cursor);
+                
+
                 for (int i=0; i<8; i++){
                     for (int j=0; j<8; j++){
                         if(MAPA[i][j]==0){ //Se for 0, desenha a peça laranha
-                            al_draw_bitmap(dlaranja,(j*q)+OFFSETX+3,(i*q)+OFFSETY+3,0);//função desenha
+                            al_draw_bitmap(dlaranja,j*q,i*q,0);//função desenha
                         }
                         if(MAPA[i][j]==1){ //Se for 1, desenha a peça roxa;
                            al_draw_bitmap(droxo,(j*q)+OFFSETX+3,(i*q)+OFFSETY+3,0); //função desenha
@@ -248,6 +309,14 @@ int desinicializa(){
     al_destroy_display(display);
     al_destroy_event_queue(event_queue);
     al_destroy_display(display);
+    al_destroy_bitmap(Pirulito);
+    al_destroy_bitmap(fundorosa);
+    al_destroy_bitmap(Festa);
+    al_destroy_bitmap(fundosugar);
+    al_destroy_bitmap(Maozinha);
+    al_destroy_bitmap(Muda1);
+    al_destroy_bitmap(Nave);
+    al_destroy_bitmap(fundo);
     return 0;
 }
 
@@ -276,7 +345,7 @@ int mainmenu(){
        
     if(event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && event.mouse.button == 1){
         if(keyboardState == 1 /* temporário ->*/ || keyboardState == 2 || keyboardState == 3 || keyboardState == 0){
-            jogo(event);
+        jogo(event);
         }
     }
     //if keyboardState == 2 { segundaFase() }
@@ -299,7 +368,10 @@ int main(int argc, char **argv){
     while(!sair){
         mainmenu();
     }
+    
+    
     desinicializa();
+                
+    
     return 0;
 }
-
