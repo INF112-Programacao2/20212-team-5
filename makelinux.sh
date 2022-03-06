59b843db93d1f0e273b9965f99abcfefae7a66b7
Gabriel <gabrieltsf10@gmail.com>
2022-03-06 18:57:10 -0300

    ajustando gitignore

--- makelinux.sh
+++
@@ -1,5 +0,0 @@
-if [[ -d release ]]; then 
-    cd src && g++ -o candycrush.out main.cpp -lallegro -lallegro_image -lallegro_ttf -lallegro_font && mv candycrush.out  ../release && cp -r ../src/assets ../release
-else
-    mkdir release && cd src && g++ -o candycrush.out main.cpp -lallegro -lallegro_image -lallegro_ttf -lallegro_font && mv candycrush.out  ../release && cp -r ../src/assets ../release
-fi
