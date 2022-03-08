f0b7fc1bb1861c4cd382fdc5285dab8d79bc6f06
Pedro Moura <pedroh.moura37@gmail.com>
2022-03-07 22:25:52 -0300

    Mapeamento para o botao iniciar e correcao no nome da imagem botao

--- src/menu.cpp
+++ src/menu.cpp
@@ -14,10 +14,15 @@ int mainmenu(){
     ALLEGRO_EVENT event;                        //INICIA UMA ÚNICA FILA DE EVENTOS
     Nivel nivel;
     
+    int botao_x = 600;
+    int botao1_y = 220;
+    int botao2_y = 320;
+    
+
     al_draw_bitmap(fundo,0,0,0);
     al_draw_bitmap(logo,0,50,0);
-    al_draw_bitmap(botao,600,220,0);
-    al_draw_bitmap(botao,600,320,0);
+    al_draw_bitmap(botao,botao_x,botao1_y,0);
+    al_draw_bitmap(botao,botao_x,botao2_y,0);
     
 
     al_flip_display();
@@ -34,6 +39,11 @@ int mainmenu(){
     }
 
     if(event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && event.mouse.button == 1){
+        /* if (mouse_x >= botao_x &&
+            mouse_x <= botao_x + al_get_bitmap_width(botao)-30 &&
+            mouse_y >= botao1_y &&
+            mouse_y <= botao1_y + al_get_bitmap_height(botao)-30)  mapeando primeiro botão, para ser o "INICIAR"*/
+        std::cout << "Click no menu!" << std::endl;
         if(keyboardState == 1 /* temporário ->*/ || keyboardState == 2 || keyboardState == 3 || keyboardState == 0){
         sorteia(MAPA);
         nivel.faseUm(event);
