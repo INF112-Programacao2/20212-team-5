6bd3e4cb9d6cad6df2bd76ba8eff1abdae5d487f
Gabriel <gabrieltsf10@gmail.com>
2022-03-08 20:51:15 -0300

    Adicionando estrutura para a classe pontuação

--- src/pontuacao.cpp
+++ src/pontuacao.cpp
@@ -1,11 +1,53 @@
 #ifndef PONTUACAO_HPP
 #define PONTUACAO_HPP
 
+#include "allegro.cpp"
 #include "pontuacao.hpp"
+
+
 
 int Pontos::getPontuacao(){
     int pontuacao = this->pontuacao;
     return pontuacao;
 }
+
+int Pontos::addPontuacao(int i){
+    this->pontuacao += i;
+    return this->pontuacao;
+}
+
+void Pontos::removePontuacao(int i){
+    this->pontuacao -= i;
+}
+
+int Pontos::getObjRestantes(){
+    return this->objrestantes;
+}
+
+void Pontos::setObjRestantes(int i){
+    this->objrestantes = i;
+}
+
+void Pontos::diminuirObjRestantes(){
+    this->objrestantes -= 1;
+}
+
+void Pontos::aumentarObjRestantes(){
+    this->objrestantes += 1;
+}
+
+void Pontos::escrevePontuacao(ALLEGRO_FONT *font){
+    al_draw_textf(font, al_map_rgb(255,0,0),330,520, 0, "%d", this->pontuacao);   
+}
+
+void Pontos::escreveObjRestantes(ALLEGRO_FONT *font){
+    al_draw_textf(font, al_map_rgb(255,0,0),330,520, 0, "%d", this->pontuacao);   
+}
+
+void Pontos::escreveMovRestantes(ALLEGRO_FONT *font){
+    al_draw_textf(font, al_map_rgb(255,0,0),330,520, 0, "%d", this->pontuacao);   
+}
+
+
 
 #endif 
