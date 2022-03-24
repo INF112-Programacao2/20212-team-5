478e093e62c31d2dfe064ac56c311440bb39dfc1
Gabriel <67779348+GabrielTheophilo@users.noreply.github.com>
2022-03-24 11:01:57 -0300

    Revert "Adicionando lógica de Banco de Dados (funcionando mas sem corpo das outras funções mais críticas)"

--- src/main.cpp
+++ src/main.cpp
@@ -3,45 +3,16 @@
             Trabalho final de INF112, consiste em recriar o game CandyCrush em C++, disponível para computadores Windows/Linux x64
                     FLAGS NECESSÁRIAS PARA O COMPILADOR/LINKADOR -> -lallegro -lallegro_image -lallegro_font -lallegro_ttf
 */
-#include "escreve.cpp"
+
 #include <stdlib.h>
 #include "main.hpp"
 #include "inicializa.cpp"
 #include "nivel.cpp"
 #include "menu.cpp"
-#include "escreve.cpp"
 
-
-using namespace std;
 
 int main(int argc, char **argv){
     Menu Menu;
-    
-    leArquivo();
- //   char save[] = R"(
- //       {
- //       "Save": {
- //           "Fase":  4,
- //           "Pontuação": 0,
- //           "Movimentos":  [
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1,
- //               0, 1, 1, 1, 1, 1, 1, 1
- //           ],
- //           "Morreu": false,
- //           "Poderes": [Ovni, 2], [Pirulito, 2],
- //           "Data": [21, 03, 2022]
- //       }
- //   }
- //   )";
- //   Arquivo << save;
- //   Arquivo.close();
- //   
 	if(!inicializa()) return -1;
     srand(time(NULL));
     while(!sair){
--- src/nivel.hpp
+++ src/nivel.hpp
@@ -2,24 +2,30 @@
 #define NIVEL_H
 
 #include "allegro.cpp"
-#include "pontuacao.hpp"
-#include "poder.hpp"
 
 
 class Nivel {
-    
     public:
         int faseUm(ALLEGRO_EVENT &ev);
         
 };
 
-class Mapa {
+class FaseUm : public Nivel {
+    private:
+     ////////////////////////////////
+    protected:
+     ////////////////////////////////
     public:
-        int MAPA[8][8];
-        int getMapa();
-        int getCoordenada(int x, int i);
-        void setMapa(int escolha);
+     ////////////////////////////////
 };
 
+class FaseDois : public Nivel {
+    private:
+     ////////////////////////////////
+    protected:
+     ////////////////////////////////
+    public:
+     ////////////////////////////////
+};
 
 #endif
