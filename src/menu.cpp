4833858dc58ebb23e618a4afa70117169a0b3c9d
Gabriel <gabrieltsf10@gmail.com>
2022-03-28 23:29:31 -0300

    Pontuação | Menu | Nivel | Mapa --> adicionando estrutura para vários niveis

--- src/menu.cpp
+++ src/menu.cpp
@@ -12,7 +12,10 @@ int Menu::mainmenu(){
         EXEMPLO: O jogador inicia o tabuleiro e quando mandamos um BREAK na função jogo(), o programa retorna ao menu inicial, por conta da função Main, que está em um loop
     */
     ALLEGRO_EVENT event;                        //INICIA UMA ÚNICA FILA DE EVENTOS
+    Pontos EscreverFase;
+
     Nivel nivel;
+    int fase = 0;
     
     al_draw_bitmap(fundo,0,0,0);
     al_draw_bitmap(logo,0,50,0);
@@ -25,22 +28,27 @@ int Menu::mainmenu(){
     if(event.type == ALLEGRO_EVENT_KEY_UP && event.keyboard.keycode==ALLEGRO_KEY_UP){
         if(keyboardState<3){
             keyboardState += 1;
+            EscreverFase.escreverFase(font, keyboardState);
         }
     }
     if(event.type == ALLEGRO_EVENT_KEY_UP && event.keyboard.keycode==ALLEGRO_KEY_DOWN){
         if(keyboardState>0){
             keyboardState -= 1;
+            EscreverFase.escreverFase(font, keyboardState);
         }
     }
     
 
     if(event.type == ALLEGRO_EVENT_MOUSE_BUTTON_DOWN && event.mouse.button == 1){
-        if(keyboardState == 1 /* temporário ->*/ || keyboardState == 2 || keyboardState == 3 || keyboardState == 0){
-        nivel.faseUm(event);
+        if(keyboardState == 1 /* temporário ->*/ || keyboardState == 3 || keyboardState == 0){
+            fase=1;
+            
+            nivel.faseUm(event, fase);
         }
+        if (keyboardState == 2){ fase=2;nivel.faseDois(event, fase);}
     }
     
-    //if keyboardState == 2 { segundaFase() }
+    
     //if keyboardState == 3 { terceiraFase() }
     //if keyboardState == 4 { quartaFase() }
 
--- src/nivel.hpp
+++ src/nivel.hpp
@@ -4,63 +4,17 @@
 #include "allegro.cpp"
 #include "pontuacao.hpp"
 #include "poder.hpp"
+#include "mapa.cpp"
 
-class Nivel {
+class Nivel{
     public:
-        int faseUm(ALLEGRO_EVENT &ev);
+        int faseUm(ALLEGRO_EVENT &ev, int fase);
+        int faseDois(ALLEGRO_EVENT &ev, int fase);
+        int faseTres(ALLEGRO_EVENT &ev, int fase);
+        int faseQuatro(ALLEGRO_EVENT &ev, int fase);
+        int faseCinco(ALLEGRO_EVENT &ev, int fase);
         
 };
 
-class Mapa {
-    private:
-     ////////////////////////////////
-    protected:
-     ////////////////////////////////
-    public:
-        int MAPA[18][7] = {
-            0, 2 ,3,0,5,0,0,
-            1, 5 ,3,5,6,5,6,
-            2, 1 ,6,0,5,3,6,
-        
-            3, 2 ,5,5,1,2,0,
-            3, 5 ,2,1,0,2,1,
-            5, 1 ,3,0,0,1,0,
-        
-            1, 2 ,1,0,1,2,1,
-            6, 1 ,2,6,6,5,0,
-            6, 3 ,1,3,5,1,2,
-            0, 2 ,3,0,5,0,0,
-            1, 5 ,3,5,6,5,6,
-            2, 1 ,6,0,5,3,6,
-        
-            3, 2 ,5,5,2,2,0,
-            3, 5 ,2,1,6,6,1,
-            5, 1 ,3,0,0,1,0,
-        
-            1, 2 ,1,0,1,2,1,
-            6, 1 ,2,6,6,1,0,
-            6, 3 ,1,3,5,1,2,
-        };
-        // Ordem de verificação da função check, (função especial vem antes) função 5 -> 4 -> 3
-        void funcao_check_5(int i, int j);
-        void funcao_check_4(int i, int j);
-        void funcao_check_3(int i,int j);
-        int  funcao_cair_doces(int check_type, int x, int y);
-
-        void funcao_troca_doces(int y_esq, int x_esq, int y_dir, int x_dir, Pontos &pontos);
-        int getMapa();
-        int getCoordenada(int x, int i);
-        void setMapa(int escolha);
-
-};
-
-class FaseDois : public Nivel {
-    private:
-     ////////////////////////////////
-    protected:
-     ////////////////////////////////
-    public:
-     ////////////////////////////////
-};
 
 #endif
